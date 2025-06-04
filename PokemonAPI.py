0c23378653b04a264668671811c96ecbdb33227f
Misto950 <cesarfornsviciana@gmail.com>
2025-06-04 02:09:30 +0200

    Afegit fitxer des de script Python

--- PokemonAPI.py
+++ PokemonAPI.py
@@ -1,15 +1,15 @@
 [
     {
-        "title":"One Piece: The Movie",
-        "episodes":1,
-        "score":7.09,
-        "status":"Finished Airing",
-        "year":null,
+        "title":"One Piece",
+        "episodes":null,
+        "score":8.73,
+        "status":"Currently Airing",
+        "year":1999,
         "genres":[
             "Action",
             "Adventure",
             "Fantasy"
         ],
-        "synopsis":"Many years ago, Woonan, a legendary pirate, plundered one-third of the world's gold and stashed it away on his secret island shrouded in mystery. \n\nIn the present, Luffy and the rest of the Straw Hats continue on their journey to the Grand Line. They are robbed by a group of bandits. Led by their captain, El Drago, these same bandits are headed towards Woonan's famed island. Even though the Straw Hat Pirates must now recover their stolen treasure, they have gained another objective: to discover the lost island, its treasures, and learn about the legend of Woonan.\n\n[Written by MAL Rewrite]"
+        "synopsis":"Barely surviving in a barrel after passing through a terrible whirlpool at sea, carefree Monkey D. Luffy ends up aboard a ship under attack by fearsome pirates. Despite being a naive-looking teenager, he is not to be underestimated. Unmatched in battle, Luffy is a pirate himself who resolutely pursues the coveted One Piece treasure and the King of the Pirates title that comes with it.\n\nThe late King of the Pirates, Gol D. Roger, stirred up the world before his death by disclosing the whereabouts of his hoard of riches and daring everyone to obtain it. Ever since then, countless powerful pirates have sailed dangerous seas for the prized One Piece only to never return. Although Luffy lacks a crew and a proper ship, he is endowed with a superhuman ability and an unbreakable spirit that make him not only a formidable adversary but also an inspiration to many.\n\nAs he faces numerous challenges with a big smile on his face, Luffy gathers one-of-a-kind companions to join him in his ambitious endeavor, together embracing perils and wonders on their once-in-a-lifetime adventure.\n\n[Written by MAL Rewrite]"
     }
 ]
